c3256cb254adae0f02d1fbec0212a2ea72766597
hectorlf <hectorlopezfernandez@hotmail.com>
2018-08-15 01:06:48 +0200

    Spice up the post list with a loading animation and more messages

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,22 +1,37 @@
 <template>
           <div class="card">
             <div class="card-body">
-              <div class="card-title"><a href="#" class="btn btn-primary">New Post</a></div>
+              <div class="card-title"><a @click="addPost()" class="btn btn-primary">New Post</a></div>
               <div class="table-responsive">
                 <table class="table">
                   <thead class=" text-primary">
-                    <th>Id</th>
-                    <th>Name</th>
+                    <tr>
+                      <th>Id</th>
+                      <th>Name</th>
+                    </tr>
                   </thead>
+                  <tfoot class="text-center" v-if="!isEmpty && !isLoading">
+                    <tr>
+                      <td colspan="2"><i class="fa fa-chevron-circle-left" style="opacity: 0.5"></i> 1 <i class="fa fa-chevron-circle-right" style="opacity: 0.5"></i></td>
+                    </tr>
+                  </tfoot>
                   <tbody>
                     <tr v-for="item in items" :key="item.id">
                       <td>{{ item.id }}</td>
                       <td>{{ item.name }}</td>
                     </tr>
+                    <tr v-if="isEmpty && !isLoading">
+                      <td colspan="2"><i>No posts were returned this time...</i></td>
+                    </tr>
+                    <tr v-if="isLoading">
+                      <td colspan="2"><i>Loading...</i></td>
+                    </tr>
                   </tbody>
                 </table>
               </div>
+              <div class="loader-bar" v-if="isLoading"></div>
             </div>
+            
           </div>
 </template>
 
@@ -27,25 +42,72 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class PostList extends Vue {
+  loading: boolean = false;
   items: Array<any> = [];
 
   loadData (): void {
     var vm = this;
     axios.get('post-list.json')
       .then(function (response) {
-        console.log("Aha!")
         vm.items = response.data.results;
+        vm.loading = false;
       })
       .catch(function (error) {
-        console.log("Booo!")
+        alert("An error occurred while loading posts");
+        vm.items = [];
+        vm.loading = false;
       });
   }
 
+  addPost (): void {
+    alert("New post?");
+  }
+
+  debouncedLoadData (): void {}
+
+  created () {
+    this.debouncedLoadData = _.debounce(this.loadData, 4000);
+  }
+
   mounted () {
-    this.loadData();
+    this.loading = true;
+    this.debouncedLoadData();
+  }
+
+  get isLoading (): boolean {
+    return this.loading;
+  }
+  get isEmpty (): boolean {
+    return this.items.length === 0;
   }
 }
 </script>
 
 <style>
+.loader-bar {
+  height: 4px;
+  width: 100%;
+  position: relative;
+  overflow: hidden;
+  background-color: #ddd;
+}
+.loader-bar:before{
+  display: block;
+  position: absolute;
+  content: "";
+  left: -200px;
+  width: 60px;
+  height: 4px;
+  background-color: #fbc658;
+  animation: loader-bar 2s linear infinite;
+}
+@keyframes loader-bar {
+    from {left: -200px; width: 0%;}
+    30% {width: 10%;}
+    50% {width: 50%;}
+    70% {width: 70%;}
+    80% {left: 50%;}
+    95% {left: 120%;}
+    to {left: 100%;}
+}
 </style>
