0ed4f123fb1fa9d09dae0ece277fd76cf8fa1fcf
Hector Lopez <hectorlopezfernandez@hotmail.com>
2020-12-25 11:16:19 +0100

    Fix chaining of operations on save
    Plus, start implementing loading state control.

--- src/TagEditor.svelte
+++ src/TagEditor.svelte
@@ -17,6 +17,7 @@
 
   const endpointUrl = baseEndpointUrl + '/tags';
   let editing = !!id;
+  let loading = true;
   let tag = {};
 
   function load() {
@@ -26,32 +27,34 @@
           tag = response.data;
         }).catch(error =>
           console.log("Error loading: " + error)
+          // show a popup error message
         );
     }
   }
 
   function save() {
     if (editing) {
-      console.log(tag)
-      axios.put(endpointUrl + '/' + id, tag)
+      return axios.put(endpointUrl + '/' + id, tag)
         .then(response => {
           console.log("Successfully updated tag: " + id);
-        }).catch(error =>
-          console.log("Error saving: " + error)
-        );
+        });
     } else {
-      axios.post(endpointUrl, tag)
+      return axios.post(endpointUrl, tag)
         .then(response => {
           console.log("Successfully created tag: " + tag.name);
-        }).catch(error =>
-          console.log("Error saving: " + error)
-        );
+        });
     }
   }
 
   function handleSave() {
-    save();
-    window.location = listUrl;
+    save()
+      .then(response => {
+        window.location = listUrl;
+      })
+      .catch(error => {
+        console.log("Error saving: " + error);
+        // show a popup error message
+      });
   }
 
   function handleCancel() {
@@ -60,6 +63,7 @@
 
   onMount(async () => {
     load();
+    loading = false;
   });
 </script>
 
@@ -68,20 +72,20 @@
     <div class="col-lg-12">
       <div class="form-group">
         <label for="name">Name</label>
-        <input class="form-control" id="name" name="name" placeholder="A sample tag" type="text" bind:value={tag.name}>
+        <input id="name" name="name" type="text" bind:value={tag.name} disabled={loading} class="form-control" placeholder="A sample tag">
         <small class="text-muted">This text will be escaped automatically when presented on the page</small>
       </div>
     </div>
     <div class="col-lg-12">
       <div class="form-group">
         <label for="name">Slug</label>
-        <input class="form-control" id="slug" name="slug" placeholder="a-sample-tag" type="text" bind:value={tag.slug}>
+        <input id="slug" name="slug" type="text" bind:value={tag.slug} disabled={loading} class="form-control" placeholder="a-sample-tag">
         <small class="text-muted">This text is used in URLs, e.g. /tags/a-sample-tag, and must be URL-encoded</small>
       </div>
     </div>
   </div>
   <div class="form-actions btn-list">
-    <button class="btn btn-primary btn-rounded" type="button" on:click={handleSave}>{translations.save}</button>
-    <button class="btn btn-default btn-rounded" type="button" on:click={handleCancel}>{translations.cancel}</button>
+    <button on:click={handleSave} disabled={loading} class="btn btn-primary btn-rounded" type="button">{translations.save}</button>
+    <button on:click={handleCancel} disabled={loading} class="btn btn-default btn-rounded" type="button">{translations.cancel}</button>
   </div>
 </form>
